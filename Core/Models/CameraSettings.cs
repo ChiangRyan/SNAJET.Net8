f24e2f27fc788c6c2345fe6ecdbc965994adc93a
ChiangRyan <jjs0613134571@gmail.com>
2026-04-23 16:50:42 +0800

    Revert "回復"
    This reverts commit d5d3ec7fa561dc42f395ca5785acdc9cf38ba9ec.

--- Core/Models/CameraSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace SANJET.Core.Models
-{
-    public class CameraSettings
-    {
-        public string RtspUrl { get; set; } = string.Empty;
-        public bool EnableHardwareDecoding { get; set; } = true;
-        public int NetworkCachingMs { get; set; } = 300;
-    }
-}
